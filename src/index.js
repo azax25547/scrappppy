d6923ff77a7b09165d19421828c08ee4fe65d306
azax25547 <dtmishra43@gmail.com>
2022-11-27 03:28:51 +0530

    Added Morgan

--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ const { affairRouter } = require('./routes/affairs.route');
 require('dotenv').config();
 const cors = require('cors');
 
-const { numberRouter } = require('./routes/number.routes')
+const { numberRouter } = require('./routes/number.routes');
+const morgan = require('morgan');
 
 const app = express()
 const PORT = process.env.PORT || 5000;
 
 app.use(cors())
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms'))
 app.use(express.json())
 
 app.get('/stat', (req, res) => {
